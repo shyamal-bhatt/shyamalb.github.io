1c9f81b211c328d5814b0c4cd54e86722a1e95ad
shyamal-bhatt <shyamalbhatt20@gmail.com>
2024-01-03 18:26:09 -0500

    First Commit; Pushing the Frontend

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = (socialLinks) => {
       <div className="bg-primary">
         <div className="container flex flex-col justify-between py-6 sm:flex-row">
           <p className="text-center font-body text-white md:text-left">
-            © Copyright 2022. All right reserved.
+            © 2024 Shyamal Bhatt. All right reserved.
           </p>
           <div className="flex items-center justify-center pt-5 sm:justify-start sm:pt-0">
             <a href={socialLinks.github} className="cursor-pointer" target="_blank">
